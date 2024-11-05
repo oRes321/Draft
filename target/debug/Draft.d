f2a262efbb3e402afcedcbb5e6b4dacdc90ca6ab
oRes321 <oreschaos4@gmail.com>
2024-11-05 19:02:58 +0200

    Task

---
+++ target/debug/Draft.d
@@ -0,0 +1,1 @@
+H:\Rust_PROJECT\Draft\target\debug\Draft.exe: H:\Rust_PROJECT\Draft\src\main.rs
---
+++ target/debug/deps/Draft.d
@@ -0,0 +1,5 @@
+H:\Rust_PROJECT\Draft\target\debug\deps\Draft.exe: src/main.rs
+
+H:\Rust_PROJECT\Draft\target\debug\deps\Draft.d: src/main.rs
+
+src/main.rs:
